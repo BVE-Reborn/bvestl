b52b65f15f337d33a1aee9b2c0489f29a1bccd60
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-05-05 16:10:26 -0400

    Update vcpkg port to v0.2

--- data/vcpkg/ports/eastl-polyalloc/portfile.cmake
+++ data/vcpkg/ports/eastl-polyalloc/portfile.cmake
@@ -3,8 +3,8 @@ include(vcpkg_common_functions)
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO BVE-Reborn/eastl-polyalloc
-    REF v0.1
-    SHA512 1ab7b3f06806e8ae387cb78e95af5eed4fe3258ed8f1b045f7ac69bda7dab4cd9a5faf122fadf96715d3a09f90cb5de27a80350575f75329f7ec2df746241c2a
+    REF v0.2
+    SHA512 22a9236f8baa089c96122b84d1ddaf5d5d15b71ecd1919f7059388903034decc816545a1c48baa009f38fbc60bbb378ca39a8dc3e4efac8e9801453241d86696
     HEAD_REF master
 )
 
