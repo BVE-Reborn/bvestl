f8428ec4b41b6ea836a931eb3d044563d5bb61fa
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-05-05 16:03:50 -0400

    Stay consistent with EASTL naming conventions

--- include/EASTL/polyalloc/polyalloc.h
+++ include/EASTL/polyalloc/polyalloc.h
@@ -5,28 +5,28 @@
 #include <EASTL/internal/config.h>
 
 namespace eastl::polyalloc {
-	class Allocator {
+	class allocator {
 	  public:
-		Allocator() = default;
-		Allocator(Allocator const&) = delete;
-		Allocator(Allocator&&) = delete;
-		Allocator& operator=(Allocator const&) = delete;
-		Allocator& operator=(Allocator&&) = delete;
+		allocator() = default;
+		allocator(allocator const&) = delete;
+		allocator(allocator&&) = delete;
+		allocator& operator=(allocator const&) = delete;
+		allocator& operator=(allocator&&) = delete;
 
 		virtual void* allocate(size_t n, int flags = 0) = 0;
 		virtual void* allocate(size_t n, size_t alignment, size_t offset, int flags = 0) = 0;
 		virtual void deallocate(void* p, size_t n) = 0;
 	};
 
-	class AllocatorHandle {
+	class allocator_handle {
 	  public:
-		EA_FORCE_INLINE AllocatorHandle(Allocator* allocator, char const* = nullptr) : allocator_(allocator) {}
+		EA_FORCE_INLINE allocator_handle(Allocator* allocator, char const* = nullptr) : allocator_(allocator) {}
 
-		EA_FORCE_INLINE AllocatorHandle(const AllocatorHandle& x, const char* = nullptr) : allocator_(x.allocator_) {}
+		EA_FORCE_INLINE allocator_handle(const allocator_handle& x, const char* = nullptr) : allocator_(x.allocator_) {}
 
-		EA_FORCE_INLINE ~AllocatorHandle() = default;
+		EA_FORCE_INLINE ~allocator_handle() = default;
 
-		EA_FORCE_INLINE AllocatorHandle& operator=(const AllocatorHandle& x) = default;
+		EA_FORCE_INLINE allocator_handle& operator=(const allocator_handle& x) = default;
 
 		EA_FORCE_INLINE void* allocate(size_t n, int flags = 0) {
 			return allocator_->allocate(n, flags);
@@ -46,15 +46,15 @@ namespace eastl::polyalloc {
 
 		EA_FORCE_INLINE void set_name(const char*) {}
 
-		EA_FORCE_INLINE bool operator==(AllocatorHandle const& rhs) const {
+		EA_FORCE_INLINE bool operator==(allocator_handle const& rhs) const {
 			return allocator_ == rhs.allocator_;
 		}
 
-		EA_FORCE_INLINE bool operator!=(AllocatorHandle const& rhs) const {
+		EA_FORCE_INLINE bool operator!=(allocator_handle const& rhs) const {
 			return !(rhs == *this);
 		}
 
 	  private:
-		Allocator* allocator_;
+		allocator* allocator_;
 	};
 }
