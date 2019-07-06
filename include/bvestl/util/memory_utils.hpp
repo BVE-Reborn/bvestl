e68beb71b1320804023ca912acd699483f0434df
David Dubois <dexcelstraun7@gmail.com>
2019-07-05 18:05:39 -0700

    Fuck everything

--- include/bvestl/util/memory_utils.hpp
+++ include/bvestl/util/memory_utils.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#include "foundational/allocation/bytes.hpp"
+#include "bvestl/polyalloc/bytes.hpp"
 
 /*!
  * \brief Some useful utilities
  */
 
-namespace foundational::allocation {
-    constexpr EA_FORCE_INLINE Bytes align(const Bytes value, const Bytes alignment) noexcept {
-        return ((value + (alignment - 1)) & ~(alignment - 1));
-    }
-} // namespace foundational::allocation
+namespace bvestl {
+	namespace polyalloc {
+		constexpr EA_FORCE_INLINE Bytes align(const Bytes value, const Bytes alignment) noexcept {
+			return ((value + (alignment - 1)) & ~(alignment - 1));
+		}
+	}
+}
